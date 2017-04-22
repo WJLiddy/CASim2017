4583a5beffc30f80d09e8874a6f15dc803240b90
Jacob Liddy <liddyjacob@gmail.com>
2017-04-22 19:24:27 -0400

    Test script now apart from Class declarations!

--- CASim2017/server/Sculpture.py
+++ CASim2017/server/Sculpture.py
@@ -91,30 +91,4 @@ class Gallery:
 
 
 
-print(time.time())
 
-#Gallery of size 5
-gallery = Gallery(5)
-
-gallery.push(4, "Great piece");
-gallery.rate(4, 3);
-gallery.rate(4, 4);
-gallery.rate(4, 4);
-gallery.push(6, "Darn not good");
-gallery.rate(6, 2);
-gallery.rate(6, 1.5);
-gallery.push(16, "This is CONTROVERISAL");
-gallery.rate(16, 0);
-gallery.rate(16, 4);
-gallery.rate(16, 0);
-gallery.rate(16, 4);
-gallery.push(12, "Shit drawing");
-gallery.rate(12,1);
-gallery.rate(12,0);
-gallery.rate(12,0);
-gallery.push(1, "Ya, okay");
-gallery.rate(1, 2);
-gallery.rate(1, 2);
-gallery.push(1, "NEW piece");
-
-gallery.test_display();
