f2113d5af7a8b6411c7002ea8f7551d6a9449c1b
CorruptData <mccargishm@gmail.com>
2017-04-23 12:16:22 -0400

    why

--- CASim2017/Assets/CameraControl.cs
+++ CASim2017/Assets/CameraControl.cs
@@ -13,34 +13,46 @@ public class CameraControl : MonoBehaviour {
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+    public float rot = 0.0F;
 
     void Update () {
         yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedV * Input.GetAxis("Mouse Y");
+        
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        rot += rotation;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
 
         if (Input.GetKey(KeyCode.A))
         {
-            posX -= (float)Math.Cos((yaw * Math.PI) / 180.0f) * speedMov;
-            posZ += (float)Math.Sin((yaw * Math.PI) / 180.0f) * speedMov;
+            posX -= (float)Math.Cos(rot) * speedMov;
+            posZ += (float)Math.Sin(rot) * speedMov;
         }
 
          
         if (Input.GetKey(KeyCode.W))
         {
-           posX += (float)Math.Sin((yaw * Math.PI) / 180.0f) * speedMov;
-           posZ += (float)Math.Cos((yaw * Math.PI) / 180.0f) * speedMov;
+           posX += (float)Math.Sin(rot) * speedMov;
+           posZ += (float)Math.Cos(rot) * speedMov;
         }
  
         if (Input.GetKey(KeyCode.S))
         {
-           posX -= (float)Math.Sin((yaw * Math.PI) / 180.0f) * speedMov;
-           posZ -= (float)Math.Cos((yaw * Math.PI) / 180.0f) * speedMov;
+           posX -= (float)Math.Sin(rot) * speedMov;
+            posZ -= (float)Math.Cos(rot) * speedMov;
         }
  
         if (Input.GetKey(KeyCode.D))
         {
-            posX += (float)Math.Cos((yaw * Math.PI) / 180.0f) * speedMov;
-            posZ -= (float)Math.Sin((yaw * Math.PI) / 180.0f) * speedMov;
+            posX += (float)Math.Cos(rot) * speedMov;
+            posZ -= (float)Math.Sin(rot) * speedMov;
         }
 
         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
